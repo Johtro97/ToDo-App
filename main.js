5f30af31c0ca4ffe312c6dd28e9178ab586cf7f9
Johanna <112203192+Johtro97@users.noreply.github.com>
2023-06-25 12:03:53 +0200

    add todo, delete todo

--- main.js
+++ main.js
@@ -3,10 +3,23 @@ const addBtn = document.querySelector("#btn-add");
 const taskList = document.querySelector("#task-list")
 const form = document.querySelector("#task-form")
 const createForm = document.getElementById("create-form");
-let todos = [];
+let todos = []; 
+let idCounter = 9
+
+//add users input to task list
+form.addEventListener("submit", (event) => {
+    if (input.value === ''){
+      alert("Write someting first!");
+      event.preventDefault();
+  }else {
+    event.preventDefault();
+    let description = form.children[0];
+    displayTodo(description.value, todos);
+    description.value = "";
+  }});
 
 //get all todos from api
-function getAllTodos() {
+function fetchTodos() {
     fetch("https://dummyjson.com/todos?limit=8&skip=0")
       .then((res) => res.json())
       .then((values) => {
@@ -25,44 +38,82 @@ function createTodos(todos) {
 
 function displayTask(todo) {
   let li = document.createElement("li");
-  li.classList.add("todo-item");
   let article = document.createElement("article");
+  article.setAttribute("id", "article")
 
   let description = document.createElement("p");
   let completed = document.createElement("input");
   let removeBtn = document.createElement("button");
+
 
   completed.type = "checkbox";
   description.innerText = todo.todo;
   completed.checked = todo.completed;
   removeBtn.innerText = "X";
 
-  //line-through text if task is completed
+  //line-through text if task is completed  
   if(completed.checked){
-    li.style.textDecoration = "line-through"
+    description.style.textDecoration = "line-through" 
   }else{
-    li.style.textDecoration = "none"
+    description.style.textDecoration = "none"
   }
 
+  //if user click on checkbock
   completed.addEventListener("change", (event) => {
     if(event.target.checked){
-      li.style.textDecoration = "line-through"
+        description.style.textDecoration = "line-through"
     }else{
-      li.style.textDecoration = "none"
+      description.style.textDecoration = "none"
     }
   });
 
+  //if user click on removeBtn
   removeBtn.addEventListener("click", () => {
-    removeTodo(todo, todos, () => {
-      createTodos(todos);
-    });
-  });
+    removeTodo(todo, todos)//call function to remove the task
+    }); 
 
-  article.append(description, completed, removeBtn);
+  article.append(completed, description, removeBtn);
 
   li.append(article);
   return li;
 }
 
+
+//add new todo
+function displayTodo(description, todos) {
+    fetch("https://dummyjson.com/todos/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        todo: description,
+        completed: false,
+        userId: 1,
+      }),
+    })
+      .then((res) => res.json())
+      .then((value) => {
+        value.id = idCounter++; //add id to todo
+        todos.push(value); //add new todo to array
+        createTodos(todos); //update task list
+      });
+  }
+
+ //delete a todo from tasklist
+function removeTodo(todo, todos) {
+    fetch("https://dummyjson.com/todos/" + todo.id, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((value) => {
+        if (value.isDeleted) {
+          let index = todos.findIndex((todo) => todo.id === value.id);
+          if (index !== -1) {
+            todos.splice(index, 1); //remove task from array
+          }
+          createTodos(todos); //update task list
+        }
+      });
+  }
+
 //call function to get all todos from api
-getAllTodos();
+fetchTodos();
